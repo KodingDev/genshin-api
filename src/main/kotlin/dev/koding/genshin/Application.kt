39fb18fd01479eb5c65262b0286a173ae2ff1436
Koding Dev <kodingdev@pm.me>
2021-07-28 00:36:58 +1000

    :bug: Fixed custom port property

--- src/main/kotlin/dev/koding/genshin/Application.kt
+++ src/main/kotlin/dev/koding/genshin/Application.kt
@@ -7,7 +7,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = System.getenv("server.port")?.toIntOrNull() ?: 8080, host = "0.0.0.0") {
+    embeddedServer(Netty, port = System.getProperty("server.port")?.toIntOrNull() ?: 8080, host = "0.0.0.0") {
         configureRouting()
         configureHTTP()
         configureSerialization()
